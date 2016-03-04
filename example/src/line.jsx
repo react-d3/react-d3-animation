e74eedea2ac31ab14342b13a3d14d1c8f556b809
chilijung <chilijung@gmail.com>
2016-03-04 19:27:32 +0800

    update example

--- example/src/line.jsx
+++ example/src/line.jsx
@@ -9,64 +9,71 @@ var LineChart = require('react-d3-basic').LineChart;
 var data1 = require('../data/user.json');
 var data2 = require('../data/user2.json')
 var chartSeries = [
-    {
-      field: 'age',
-      name: 'Age',
-      color: '#ff7f0e',
-      style: {
-        "strokeWidth": 2,
-        "strokeOpacity": .2,
-        "fillOpacity": .2
-      }
+  {
+    field: 'age',
+    name: 'Age',
+    color: '#ff7f0e',
+    style: {
+      "strokeWidth": 2,
+      "strokeOpacity": .2,
+      "fillOpacity": .2
     }
-  ],
-  x = function(d) {
-    return d.index;
   }
+]
+var chartSeries2 = [
+  {
+    field: 'age',
+    name: 'Age',
+    color: 'green',
+    style: {
+      "strokeWidth": 5,
+      "strokeOpacity": 1,
+      "fillOpacity": .2
+    }
+  }
+]
+,
+x = function(d) {
+  return d.index;
+}
 
 module.exports = React.createClass({
   getInitialState: function() {
     return {
-      width: 600,
-      height: 400,
-      series: chartSeries,
-      data: data1
+      chartSeries: chartSeries,
+      data: data1,
+      position: 1
     }
   },
   onClick: function() {
-    this.setState({
-      width: this.state.width === 600? 400: 600,
-      height: this.state.width === 600? 600: 400,
-      series: this.state.width === 600? [{
-        field: 'age',
-        name: 'Age',
-        color: 'green',
-        style: {
-          "stroke-width": 5,
-          "stroke-opacity": 1,
-          "fill-opacity": .2
-        }
-      }]: chartSeries,
-      data: this.state.width === 600? data2: data1
-    })
+    if(this.state.position === 1) {
+      this.setState({
+        data: data2,
+        chartSeries: chartSeries2,
+        position: 2
+      })
+    }else {
+      this.setState({
+        data: data1,
+        chartSeries: chartSeries,
+        position: 1
+      })
+    }
   },
   render: function() {
     return (
       <div>
         <button onClick={this.onClick}>toggle</button>
         <Animation
-          width= {this.state.width}
-          height= {this.state.height}
-          chartSeries= {this.state.series}
+          chartSeries= {this.state.chartSeries}
           data= {this.state.data}
           x= {x}
-          delay= {1000}
         >
           {function(state) {
             return (
               <LineChart
-                width= {state.width}
-                height= {state.height}
+                width= {500}
+                height= {500}
                 data= {state.data}
                 chartSeries= {state.chartSeries}
                 x= {x}
